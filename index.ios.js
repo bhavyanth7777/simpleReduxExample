3fea1a475d0c49b80efb7b0aebcc946fa1aee472
Bhavyanth Kolli <bhavyanth.kolli@gmail.com>
2017-02-20 12:40:28 +0530

    Clarity Redux

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,7 @@
  * @flow
  */
 
+// ARSID
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,42 +13,50 @@ import {
   View
 } from 'react-native';
 
+// Step 1. Import createStore from redux 
+import { createStore } from 'redux';
+
+// Step 2. Create Action types
+const actionTypes = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+}
+
+// Step 3. Create reducer
+const reducer = (state, action) => {
+  if(action.type === actionTypes.INCREMENT){
+    return { count: state.count+1}
+  }
+  else if (action.type === actionTypes.DECREMENT){
+    return {count: state.count -1}
+  }
+  return state
+}
+
+// Step 4. Create initial State 
+const initialState = {
+  count: 0
+}
+
+// Step 5. Create Store 
+const store = createStore(reducer, initialState)
+
 export default class simpleReduxExample extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Text style={{fontSize: 100}}>
+    {store.getState().count}
+  </Text>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// Step 6. Dispatch actions to store
+store.dispatch({type: actionTypes.INCREMENT})
+store.dispatch({type: actionTypes.INCREMENT})
+store.dispatch({type: actionTypes.DECREMENT})
+store.dispatch({type: actionTypes.INCREMENT})
+
+
 
 AppRegistry.registerComponent('simpleReduxExample', () => simpleReduxExample);
